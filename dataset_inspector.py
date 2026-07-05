94090c3ba064a325dfa99dfb4d6972565e4b35f4
Adhya Agrawal <educationpes2029@gmail.com>
2026-07-05 21:07:59 +0530

    Update dataset_inspector.py

--- dataset_inspector.py
+++ dataset_inspector.py
@@ -1,29 +1,11 @@
-<<<<<<< HEAD
 from pathlib import Path
+
 dataset_path = Path("Fruit Freshness Dataset/data/raw")
+
 for fruit_folder in dataset_path.iterdir():
     if fruit_folder.is_dir():
-
         print(f"\n{fruit_folder.name}")
-
-        for freshness_folder in fruit_folder.iterdir(): #Proceed only if this item is a directory (folder).
-
+        for freshness_folder in fruit_folder.iterdir():  # Proceed only if this item is a directory (folder).
             if freshness_folder.is_dir():
-                image_count = len(list(freshness_folder.glob("*"))) #Give me everything inside this folder, len() can count
-
+                image_count = len(list(freshness_folder.glob("*")))  # Give me everything inside this folder, len() can count
                 print(f"   {freshness_folder.name}: {image_count} images")
-=======
-from pathlib import Path
-dataset_path = Path("Fruit Freshness Dataset/data/raw")
-for fruit_folder in dataset_path.iterdir():
-    if fruit_folder.is_dir():
-
-        print(f"\n{fruit_folder.name}")
-
-        for freshness_folder in fruit_folder.iterdir(): #Proceed only if this item is a directory (folder).
-
-            if freshness_folder.is_dir():
-                image_count = len(list(freshness_folder.glob("*"))) #Give me everything inside this folder, len() can count
-
-                print(f"   {freshness_folder.name}: {image_count} images")
->>>>>>> b691b143c65ce7a3c65f7c006142b96a3bf3d63b
